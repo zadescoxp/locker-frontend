c6539705b3995df4f8f0a982d0ca0b14973a82c7
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-12-29 16:48:43 +0530

    env setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    LOCK_SECRET: process.env.LOCK_SECRET,
+  },
 };
 
 export default nextConfig;
