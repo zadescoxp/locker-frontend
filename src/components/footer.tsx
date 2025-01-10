908ac4738f69f1b15631f581d0ae24d90323e2af
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2025-01-11 01:45:10 +0530

    footer init

---
+++ src/components/footer.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="bg-lightgrey text-grey text-center flex items-center justify-center flex-col py-10 px-5 font-light">
+      <p>
+        Made by{" "}
+        <Link
+          className="text-black"
+          href="https://zadescoxp.com"
+          target="_blank"
+        >
+          @zadescoxp
+        </Link>
+      </p>
+
+      <p>
+        Vouz is an open source project, show your love and contribution on{" "}
+        <Link
+          className="text-black"
+          href="https://github.com/zadescoxp/locker-frontend"
+          target="_blank"
+        >
+          GitHub
+        </Link>
+      </p>
+    </div>
+  );
+}
