e137af7f03b7ffd67ceb83086f96002816797429
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2025-01-11 14:17:32 +0530

    bug fixed

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,15 +1,15 @@
 import { encrypt, decrypt } from "tanmayo7lock";
 
-export function encryptObjectValues(obj: any) {
+export function encryptObjectValues(obj: Record<string, unknown>) {
   return Object.keys(obj).reduce((acc, key) => {
-    acc[key] = encrypt(obj[key as any]); // Apply decrypt to each value
+    acc[key] = encrypt(obj[key as string]); // Apply decrypt to each value
     return acc;
   }, {} as Record<string, string>);
 }
 
-export function decryptObjectValues(obj: any) {
+export function decryptObjectValues(obj: Record<string, string>) {
   return Object.keys(obj).reduce((acc, key) => {
-    acc[key] = decrypt(obj[key as any]); // Apply decrypt to each value
+    acc[key] = decrypt(obj[key as string]); // Apply decrypt to each value
     return acc;
   }, {} as Record<string, string>);
 }
